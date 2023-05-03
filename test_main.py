cc0e1309f31bc78ebc30001c682fe1868a055ca7
Sirmauricio10000 <mauricio1907_5@outlook.com>
2023-05-02 19:57:21 -0500

    Test Fibonacci no definido

--- test_main.py
+++ test_main.py
@@ -8,3 +8,6 @@ def test_primo():
 
 def test_no_primo():
     assert verificar_primo(6) == {'respuesta': False, 'validacion': 'Solicitud Exitosa'}
+
+def test_fibonacci():
+    assert fibonacci(5) == 5
