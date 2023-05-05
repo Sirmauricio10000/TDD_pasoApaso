c4bfc2eec4e8d000b0fd898f86e51a44762db6cb
Sirmauricio10000 <mauricio1907_5@outlook.com>
2023-05-05 18:45:56 -0500

    Add fibonacci test endpoint

--- test_main.py
+++ test_main.py
@@ -1,31 +1,28 @@
 import requests
 import pytest
 import json
-from main import fibonacci, helloFastApi, verificar_primo
 
-expected_primo_response_false = {"validacion": "Solicitud Exitosa", "respuesta": False}
-expected_primo_response_true = {"validacion": "Solicitud Exitosa", "respuesta": True}
+test_data_isPrime = [
+    (0, False),
+    (1, False),
+    (5, True),
+    (17,True)]
 
-test_data = [(0, expected_primo_response_false), (1, expected_primo_response_false), (5, expected_primo_response_true)]
+test_data_fibonacci = [
+    (0, 0),
+    (1, 1),
+    (5, 5),
+    (10, 55)
+]
 
-
-@pytest.mark.parametrize("num, expected", test_data)
+@pytest.mark.parametrize("num, expected", test_data_isPrime)
 def test_endpoint_isPrime(num, expected):
     response = requests.get(f'http://localhost:8000/isPrime/{num}')
     data = json.loads(response.content)
-    assert data == expected
+    assert data["respuesta"] == expected
 
-def test_primo():
-    assert verificar_primo(5) == {'respuesta': True, 'validacion': 'Solicitud Exitosa'}
-
-def test_no_primo():
-    assert verificar_primo(6) == {'respuesta': False, 'validacion': 'Solicitud Exitosa'}
-
-def test_fibonacci():
-    assert fibonacci(5) == {"respuesta": 5, "validacion": "Solicitud Exitosa"}
-
-def test_fibonacci_exceptionManaged():
-    assert fibonacci("error") == {"validacion": "Error: el valor introducido no es un numero entero"}
-
-def test_helloFastApi():
-    assert helloFastApi() == {"mensaje": "Hello FastAPI"}
+@pytest.mark.parametrize("num, expected", test_data_fibonacci)
+def test_endpoint_fibonacci(num, expected):
+    response = requests.get(f'http://localhost:8000/fibonacci/{num}')
+    data = json.loads(response.content)
+    assert data["respuesta"] == expected
